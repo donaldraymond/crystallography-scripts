5509dbd39359b63db0a6c47c437077f01b6eeb62
Donald D Raymond <donaldraymond@gmail.com>
2015-02-04 16:12:20 -0500

    Changed echo format

--- XDS_refine.sh
+++ XDS_refine.sh
@@ -18,15 +18,11 @@ exec 2> >(tee  Reprocessing.log >&2)
 
 #Function to display introduction
 function intro {
-	echo
-	echo "*********************************************************"
-	echo
-	echo "This is a bash script to reprocess data with XDS using the"
-	echo "correct spacegroup, refined geometry and fine-slicing of profiles"
-	echo "AND/OR with refined values for beam divergence and mosaicity"
-	echo 
-	echo "*********************************************************"
-	echo
+echo -e "\n*********************************************************\n
+This is a bash script to reprocess data with XDS using the
+correct spacegroup, refined geometry and fine-slicing of profiles
+AND/OR with refined values for beam divergence and mosaicity
+\n*********************************************************\n"
 }
 
 #Function to backup existing files
@@ -34,8 +30,8 @@ function copyFiles {
 	if [ -e XPARM.XDS ]; then
 		cp XPARM.XDS reprocess/XPARM.XDS_$1
 	else
-		echo;echo "Cannot find XPARMS.XDS";echo
-		echo "Process data once before running this script";echo
+		echo -e "\nCannot find XPARMS.XDS\n"
+		echo -e "Process data once before running this script\n"
 		rm -rf log reprocess
 		exit 1
 	fi
@@ -43,8 +39,8 @@ function copyFiles {
 	if [ -e INTEGRATE.LP ]; then
 		cp INTEGRATE.LP reprocess/INTEGRATE.LP_$1
 	else
-		echo;echo "Cannot find INTEGRATE.LP";echo
-		echo "Process data once before running this script";echo
+		echo -e "\nCannot find INTEGRATE.LP\n"
+		echo -e "Process data once before running this script\n"
 		rm -rf log reprocess
 		exit 1
 	fi
@@ -52,8 +48,8 @@ function copyFiles {
 	if [ -e INTEGRATE.HKL ]; then
 		cp INTEGRATE.HKL reprocess/INTEGRATE.HKL_$1
 	else
-		echo;echo "Cannot find INTEGRATE.HKL";echo
-		echo "Process data once before running this script";echo
+		echo -e "\nCannot find INTEGRATE.HKL\n"
+		echo -e "Process data once before running this script\n"
 		rm -rf log reprocess
 		exit 1
 	fi
@@ -61,8 +57,8 @@ function copyFiles {
 	if [ -e CORRECT.LP ]; then
 		cp CORRECT.LP reprocess/CORRECT.LP_$1
 	else
-		echo;echo "Cannot find CORRECT.LP";echo
-		echo "Process data once before running this script";echo
+		echo -e "\nCannot find CORRECT.LP\n"
+		echo -e "Process data once before running this script\n"
 		rm -rf log reprocess
 		exit 1
 	fi
@@ -70,8 +66,8 @@ function copyFiles {
 	if [ -e XDS_ASCII.HKL ]; then
 		cp XDS_ASCII.HKL reprocess/XDS_ASCII.HKL_$1
 	else
-		echo;echo "Cannot find XDS_ASCII.HKL";echo
-		echo "Process data once before running this script";echo
+		echo -e "\nCannot find XDS_ASCII.HKL\n"
+		echo -e "Process data once before running this script\n"
 		rm -rf log reprocess
 		exit 1
 	fi
@@ -79,8 +75,8 @@ function copyFiles {
 	if [ -e FRAME.cbf ]; then
 		cp FRAME.cbf reprocess/FRAME_$1.cbf
 	else
-		echo;echo "FRAME.cbf";echo
-		echo "Process data once before running this script";echo
+		echo -e "\nFRAME.cbf\n"
+		echo -e "Process data once before running this script\n"
 		rm -rf log reprocess
 		exit 1
 	fi
@@ -88,21 +84,21 @@ function copyFiles {
 	if [ -e XDS.INP ]; then
 		cp XDS.INP reprocess/XDS.INP_$1
 	else
-		echo "Cannot find XDS.INP";echo
+		echo -e "\nCannot find XDS.INP\n"
 		exit 1
 	fi
 
 	if [ -e GXPARM.XDS ]; then
 		cp GXPARM.XDS reprocess/GXPARM.XDS_$1
 	else
-		echo "Cannot find GXPARM.XDS";echo
+		echo -e "\nCannot find GXPARM.XDS\n"
 		exit 1
 	fi
 
 	if [ -e GXPARM.XDS ]; then
 		cp GXPARM.XDS reprocess/XPARM.XDS
 	else
-		echo "Cannot find GXPARM.XDS";echo
+		echo -e "\nCannot find GXPARM.XDS\n"
 		exit 1
 	fi
 }
@@ -110,43 +106,44 @@ function copyFiles {
 #Function to run XDS
 function run_xds {
 if [[ "$input_mode" = "s" ]] || [[ "$input_mode" = "S" ]]; then
-	echo "Running XDS in silent mode";echo
+	echo -e "Running XDS in silent mode\n"
 	xds_par > log/Reprocess_noBEAM_$1.log
 else
-	echo "Running XDS in Verbose mode";echo
+	echo -e "Running XDS in Verbose mode\n"
 	xds_par |tee log/Reprocess_noBEAM_$1.log
 fi
 }
 
 #Function to reprocess with correct spacegroup, refined geometry and fine-slicing of profiles
 function repro {
-	echo "Creating a new XDS.INP with the correct spacegroup, refined geometry and fine-slicing of profiles";echo
+	echo -e "Creating a new XDS.INP with the correct spacegroup, refined geometry and fine-slicing of profiles\n"
 	egrep -v 'JOB|REIDX' XDS.INP | egrep -v "NUMBER_OF_PROFILE_GRID_POINTS_ALONG_" > XDS.INP_new
 	echo "! JOB=XYCORR INIT COLSPOT IDXREF DEFPIX INTEGRATE CORRECT" > XDS.INP
 	echo "JOB=INTEGRATE CORRECT" >> XDS.INP
 	echo NUMBER_OF_PROFILE_GRID_POINTS_ALONG_ALPHA/BETA=13 >> XDS.INP ! default is 9
 	echo NUMBER_OF_PROFILE_GRID_POINTS_ALONG_GAMMA=13      >> XDS.INP ! default is 9
 	cat XDS.INP_new >> XDS.INP
-	echo "Reprocessing with the correct spacegroup, refined geometry and fine-slicing of profiles";echo
+	echo -e "Reprocessing with the correct spacegroup, refined geometry and fine-slicing of profiles\n"
 	run_xds $1
 	}
 
 
 #function to reprocess with refined values for beam divergence and mosaicity
 function repro_BEAM {
-	echo "Creating a new XDS.INP with refined values for beam divergence and mosaicity";echo
+	echo -e "Creating a new XDS.INP with refined values for beam divergence and mosaicity\n"
 	grep _E INTEGRATE.LP | tail -2 > x
 	grep -v _E.S.D XDS.INP >> x
 	cp x XDS.INP
 	cp XDS.INP XDS.INP_BEAM
-	echo "Reprocessing XDS with refined values for beam divergence and mosaicity";echo
+	echo -e "Reprocessing XDS with refined values for beam divergence and mosaicity\n"
 	run_xds $1
 }
 
 #function to get scaling statisticss table
 function stats {
 	echo -e "Scaling statistics $1\n"
-	egrep -B 25 "WILSON STATISTICS OF DATA SET" "$2"CORRECT.LP"$3" | egrep -A 21  "SUBSET OF INTENSITY DATA WITH SIGNAL/NOISE"
+	egrep -B 25 "WILSON STATISTICS OF DATA SET" "$2"CORRECT.LP"$3" | egrep -A 20  "SUBSET OF INTENSITY DATA WITH SIGNAL/NOISE"
+	echo -e "\n####################################################\n\n"
 }
 ###########################################################
 ### RUNNING SCRIPT
@@ -162,8 +159,8 @@ intro
 echo -n "Run XDS in Silent (S) or Verbose (V) mode? "
 while read -r -n 1 -s input_mode; do
 	if [[ $input_mode = [SsVv] ]]; then
-		[[ $input_mode = [Ss] ]] && echo && echo && echo "Running XDS in silent mode" && echo
-		[[ $input_mode = [Vv] ]] && echo && echo && echo "Running XDS in verbose mode" && echo
+		[[ $input_mode = [Ss] ]] && echo -e "\n\nRunning XDS in silent mode\n"
+		[[ $input_mode = [Vv] ]] && echo -e "\n\nRunning XDS in verbose mode\n"
 		break
 	fi
 done
@@ -176,9 +173,9 @@ while [[ "$number_1" = "fail" ]]; do
 	read reproc
 
 	if ! [ "$reproc" -eq "$reproc" ] 2> /dev/null; then
-		echo;echo "Invalid input";echo
+		echo -e "\nInvalid input\n"
 	else 
-		echo;echo "Running $reproc cycles with REFINED GEOMETRY AND FINE-SLICING OF PROFILES";echo
+		echo -e "\nRunning $reproc cycles with REFINED GEOMETRY AND FINE-SLICING OF PROFILES\n"
 		number_1="pass"
 	fi
 done
@@ -190,9 +187,9 @@ while [[ "$number_2" = "fail" ]]; do
 	read beam_reproc
 
 	if ! [ "$beam_reproc" -eq "$beam_reproc" ] 2> /dev/null; then
-		echo;echo "Invalid input";echo
+		echo -e"\nInvalid input\n"
 	else 
-		echo;echo "Running $beam_reproc cycles with REFINED VALUES FOR BEAM DIVERGENCE AND MOSAICITY";echo
+		echo -e "\nRunning $beam_reproc cycles with REFINED VALUES FOR BEAM DIVERGENCE AND MOSAICITY\n"
 		number_2="pass"
 	fi
 done
@@ -201,17 +198,17 @@ done
 #create log dir
 if [ ! -d log ]; then
     mkdir log
-	echo "Created log directory";echo
+	echo -e "Created log directory\n"
 else
-	echo "Log directory exists";echo
+	echo -e "Log directory exists\n"
 fi
 
 #create reprocess dir
 if [ ! -d reprocess ]; then
     mkdir reprocess
-	echo "Created reprocess directory";echo
+	echo -e "Created reprocess directory\n"
 else
-	echo "Reprocess directory exists";echo
+	echo -e "Reprocess directory exists\n"
 fi
 
 #########################################################
@@ -225,8 +222,7 @@ echo;stats "before refinement"
 
 #Loop for reprocessing without BEAM optimzation
 for i in $(seq -f "%02g" 1 $reproc); do
-	echo; echo "####################################################";echo
-	echo "Reprocessing with REFINED GEOMETRY AND FINE-SLICING OF PROFILES: Cycle $i of $(printf %02d $reproc)";echo
+	echo -e "Reprocessing with REFINED GEOMETRY AND FINE-SLICING OF PROFILES: Cycle $i of $(printf %02d $reproc)\n"
 	repro $i
 	stats "for cycle $i"
 	copyFiles repro_$i
@@ -234,42 +230,30 @@ done
 
 #loop for reprocessing with BEAM optimization
 for j in $(seq -f "%02g" 1 $beam_reproc); do
-	echo; echo "####################################################";echo
-	echo "Reprocessing with REFINED VALUES FOR BEAM DIVERGENCE AND MOSAICITY: Cycle $j of $(printf %02d $beam_reproc)";echo
+	echo -e "Reprocessing with REFINED VALUES FOR BEAM DIVERGENCE AND MOSAICITY: Cycle $j of $(printf %02d $beam_reproc)\n"
 	repro_BEAM $j
 	stats "for cycle $j"
 	copyFiles BEAM_$j
 done
 
-
 #Move log file to reprocess folder
 mv Reprocessing.log reprocess/ 
-echo "Reprocessing finished";echo
-
+echo -e "Reprocessing finished\n"
 
 #compare stats from different runs
-echo -e "\n\n**************************************************************
+echo -e "\n**************************************************************
 **************************************************************
-\t\t\tSUMMARY
+\t\tREFINEMENT SUMMARY
 **************************************************************
 **************************************************************\n"
 #stats from CORRECT LP before refinement
 stats "before refinement" "reprocess/" "_ini"
 
-#spacer
-echo -e "\n####################################################\n"
-
 #stats from CORRECt LP after Reprocessing with REFINED GEOMETRY AND FINE-SLICING OF PROFILES
 stats "after Reprocessing with REFINED GEOMETRY AND FINE-SLICING OF PROFILES" "reprocess/" "_repro_$i"
 
-#spacer
-echo -e "\n####################################################\n"
-
 #stats from CORRECT LP after Reprocessing with REFINED VALUES FOR BEAM DIVERGENCE AND MOSAICITY
 stats "after Reprocessing with REFINED VALUES FOR BEAM DIVERGENCE AND MOSAICITY" "reprocess/" "_BEAM_$j"
-
-#spacer
-echo -e "\n####################################################\n"
 
 #end script
 echo -e "\nScript finished\n"
