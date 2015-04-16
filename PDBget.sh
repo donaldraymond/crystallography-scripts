8738a1cc658aa456c3b07272c346cab0d52e017d
Donald D Raymond <donaldraymond@gmail.com>
2015-04-16 11:00:37 -0400

    PDBget:check for download success

--- PDBget.sh
+++ PDBget.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-#function to download file
-function get_file {
-curl -O -f "http://www.rcsb.org/pdb/files/"$pdb_id""$1""
-}
-
 #get pdb id from user
 if [[ $# == 0 ]] || [[ ${#1} != 4 ]]; then
 	echo; read -p "Please enter a valid PDB ID (e.g. 1yks): " -n 4 -e pdb_id
@@ -14,8 +9,11 @@ fi
 
 #get pdb file pro
 echo -e "\nGetting $pdb_id coordinate file from PDB\n"
-get_file ".pdb"
-#pdb_file="$pdb_id.pdb"
+curl -O -sf "http://www.rcsb.org/pdb/files/"$pdb_id".pdb"
 
 #end
-echo -e "\nScript DONE!!\n"
+if [[ "$?" != "0" ]] ; then
+    echo -e "Could not download the PDB file. Please check PDB ID and/or online status\n"
+else
+	echo -e "Download complete!!\n"
+fi
