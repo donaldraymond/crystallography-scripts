7301f6062fe883aaac63dbf0bcd69dd84c9450b4
draymond <draymond@broad.mit.edu@gp134-576.broadinstitute.org>
2018-09-20 10:29:35 -0400

    Added scripts to make cif files in phenix or grade

---
+++ cif_grade.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#script to generate ligand cif files
+#written by Donald Raymond (March 30 2018)
+
+########################################################
+
+#check if grade is installed
+if hash grade 2>/dev/null; then
+	echo -e "\nFound Grade...continuing with script"
+else
+	echo -e "\nGrade is required to run this script\n"
+	exit 1
+fi
+
+#clear screen
+clear
+
+echo '
+***************************************************************
+This script will generate ligand cif files for refinement
+***************************************************************
+'
+#Function to Ask user for information 1-prompt; 2- number of characters
+function ask {
+ read -p "$1"": " -n $2 -e val
+ echo $val
+}
+
+#get SMILES string
+SMILES_string_entered=$(ask "Insert SMILES string" 1000)
+
+#remove spaces, tabs ,etc from SMILES string
+SMILES_string=${SMILES_string_entered//[[:blank:]]/}
+echo
+
+#get ligand name 
+ligandID=$(ask "What is the ligand ID? (3 character)" 3)
+echo
+
+#get file name
+fileName_entered=$(ask "filename" 20)
+
+#remove spaces, tabs ,etc from file name
+fileName=${fileName_entered//[[:blank:]]/}
+echo
+
+echo "The SMILES string is $SMILES_string" ;echo
+
+echo "The ligand ID is $ligandID" ;echo
+
+echo "The file name is $fileName.cif" ;echo
+
+#Set obabel flag
+export BDG_TOOL_OBABEL=none
+
+#Run Grade
+grade $SMILES_string -resname $ligandID -name $fileName -ocif $fileName.cif -opdb $fileName.pdb -nomogul
+
+#remove lis file 
+rm  __* 2>/dev/null
+
+echo; echo "End of script"; echo;
+
+echo
